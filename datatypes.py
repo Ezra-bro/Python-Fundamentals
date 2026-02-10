7beb158e0991e7306aa7eb67ee315fbefb633b54
Ezra-bro <ezralunani324@gmail.com>
2026-02-10 16:27:24 +0300

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+age = 56 #integer
+temperature = 30.5 #Float
+greeting = "Hello!" #string
+isMarried = False #Boolean
+
+print("I am", age, "years old")
+print("The current body temperature is", temperature, "degree celsius")
+print(greeting, "Ezra")
+print("Are you married?", isMarried)
+
+
+
+# Data structures - Multiple values stored in a single variable
+cars = ["Toyota", "Mercedes", "Audi", "Volvo", "BMW"] #List - Ordered and Changeable
+fruits = ("Banana", "Cherry", "Mango", "Peas") #Tuple - Ordered and Unchangeable
+countries = {"England", "Netherlands", "Argentina", "France"} #Set - Unordered and Unchangeable
+student = {
+    "firstname": "John",
+    "age": 22,
+    "course" : "Cybersecurity",
+    "gender" : "Male"
+
+}
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["firstname"])
+
+#Typecasting
---
+++ variables.py
@@ -0,0 +1,15 @@
+#Variable is a container used to to store a value
+
+firstname = "Ezra"
+school = "eMobilis"
+lastname = "Lunani"
+num1 = 56
+num2 = 20
+language = "python"
+
+print( firstname)
+print(school)
+print(firstname, lastname)
+print(school, "is a coding school")
+print(num1 + num2)
+print(language, "is a web programming language")
